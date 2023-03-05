59b2a58576441bcc9b0bfcc096c74c925f046dc0
VestaNe <batasheva_oa@mail.ru>
2023-03-05 23:21:49 +0500

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let i = 0; i < table.rows.length; i++){
+    
+    let row = table.rows[i];
+
+   if(row.cells[3].dataset.available == 'true'){
+      row.classList.add('available');}
+
+   if(row.cells[3].dataset.available == 'false'){
+      row.classList.add('unavailable');}
+
+   if (!row.cells[3].dataset.available){
+      row.hidden = true;}
+   
+    if (row.cells[2].textContent === 'm'){ 
+      row.classList.add('male');}
+
+    if (row.cells[2].textContent === 'f'){
+      row.classList.add('female');}
+
+    if (row.cells[1].textContent < 18){ 
+      row.style ="text-decoration: line-through";}
+
+  }
+
 }
