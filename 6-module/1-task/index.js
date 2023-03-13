645cf2647064f8705453b9006cd07c79891a3f8b
VestaNe <batasheva_oa@mail.ru>
2023-03-13 21:18:12 +0500

    class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,42 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;       
+    this.render();
   }
+
+  render() {
+    this.elem = document.createElement('TABLE');
+
+    this.elem.insertAdjacentHTML('beforeEnd', `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+      </tr>                    
+    </thead>`);
+
+    this.elem.insertAdjacentHTML('beforeEnd', this.data.map(item => `<tbody>
+      <tr>
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+        <td><button class="remove-button">[x]</button></td>
+      </tr>                    
+    </tbody>`).join(''));
+
+    this.elem.addEventListener('click', this.onClick);
+
+    return this.elem;
+  }
+
+  onClick = (event) => {
+
+  if (event.target.className != 'remove-button') return;
+      let tr = event.target.closest('TR');
+      tr.remove();
+  }
+
 }
